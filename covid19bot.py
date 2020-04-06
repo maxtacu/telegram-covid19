0bcc98c68d1a8838b98aeb116a0fca761024f7c9
Maxim Tacu <maxim.tacu@olx.com>
2020-04-06 01:56:00 +0100

    Added top command and translations

--- covid19bot.py
+++ covid19bot.py
@@ -7,6 +7,7 @@ import sqlite3
 import time
 from datetime import datetime
 
+
 bot = telebot.TeleBot(config.telegram["token"])
 
 conn = sqlite3.connect(config.database["filename"], check_same_thread=False)
@@ -38,7 +39,9 @@ def start(message):
 @bot.callback_query_handler(func=lambda call: True)
 def iq_callback(query):
    data = query.data
-   if data.startswith('lang-'):
+   if data == 'lang-eng':
+       bot.answer_callback_query(query.id, "Please type /start again!")
+   elif data.startswith('lang-'):
         user_language_update(query.data, query.message.chat.id)
         language = language_check(query.message.chat.id)
         bot.answer_callback_query(query.id, config.translations[language]["pick"])
@@ -53,7 +56,7 @@ def user_language_update(language, user):
 def language_pick_buttons(message, language):
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('English', callback_data='lang-eng'),
+        telebot.types.InlineKeyboardButton('English', callback_data='lang-en'),
         telebot.types.InlineKeyboardButton('Русский', callback_data='lang-ru'),
         telebot.types.InlineKeyboardButton('Español', callback_data='lang-es')
     )
@@ -83,19 +86,47 @@ def stats(message):
     bot.send_message(message.chat.id, config.translations[language]["stats"].format(*stats), parse_mode="Markdown")
 
 
-# @bot.message_handler(commands=['top'])
-# def top(message):
-#     now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-#     # language = language_check(message.chat.id)
-#     c = conn.cursor()
-#     with conn:
-#         c.execute(f"""UPDATE users SET last_check='{now}' WHERE user_id=={message.chat.id}""")
-#     with conn:
-#         *stats, = c.execute("SELECT * FROM countries LIMIT 5").fetchall()
-#         print(*stats)
-#     for country in *stats:
+@bot.message_handler(commands=['topcases'])
+def top_confirmed(message):
+    now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    language = language_check(message.chat.id)
+    top_stats_full_message = config.translations[language]["topconfirmed"] + '\n\n'
+    c = conn.cursor()
+    with conn:
+        c.execute(f"""UPDATE users SET last_check='{now}' WHERE user_id=={message.chat.id}""")
+    with conn:
+        *stats, = c.execute("SELECT * FROM countries ORDER BY cases DESC LIMIT 10").fetchall()
+    for country_stats in stats:
+        top_stats_full_message += config.translations["bycountry"].format(countryname=country_stats[0], cases=country_stats[1])
+    bot.send_message(message.chat.id, top_stats_full_message, parse_mode="Markdown")
 
-#     # bot.send_message(message.chat.id, config.translations[language]["stats"].format(*stats), parse_mode="Markdown")
+@bot.message_handler(commands=['toprecovered'])
+def top_recovered(message):
+    now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    language = language_check(message.chat.id)
+    top_stats_full_message = config.translations[language]["toprecovered"] + '\n\n'
+    c = conn.cursor()
+    with conn:
+        c.execute(f"""UPDATE users SET last_check='{now}' WHERE user_id=={message.chat.id}""")
+    with conn:
+        *stats, = c.execute("SELECT * FROM countries ORDER BY recovered DESC LIMIT 10").fetchall()
+    for country_stats in stats:
+        top_stats_full_message += config.translations["bycountry"].format(countryname=country_stats[0], cases=country_stats[3])
+    bot.send_message(message.chat.id, top_stats_full_message, parse_mode="Markdown")
+
+@bot.message_handler(commands=['topdeaths'])
+def top_deaths(message):
+    now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    language = language_check(message.chat.id)
+    top_stats_full_message = config.translations[language]["topdeaths"] + '\n\n'
+    c = conn.cursor()
+    with conn:
+        c.execute(f"""UPDATE users SET last_check='{now}' WHERE user_id=={message.chat.id}""")
+    with conn:
+        *stats, = c.execute("SELECT * FROM countries ORDER BY deaths DESC LIMIT 10").fetchall()
+    for country_stats in stats:
+        top_stats_full_message += config.translations["bycountry"].format(countryname=country_stats[0], cases=country_stats[2])
+    bot.send_message(message.chat.id, top_stats_full_message, parse_mode="Markdown")
 
 
 def check_user(userid, username=None):
@@ -105,13 +136,12 @@ def check_user(userid, username=None):
         user = c.execute(f"""SELECT user_id FROM users WHERE user_id == '{userid}'""").fetchone()
     if not user:
         with conn:
-            c.execute(f"INSERT INTO users VALUES ('{userid}', '{username}', '{time.strftime('%d-%m-%Y')}', '{now}', 'lang-eng')")
+            c.execute(f"INSERT INTO users VALUES ('{userid}', '{username}', '{time.strftime('%d-%m-%Y')}', '{now}', 'lang-en')")
         logger.info(f"New user detected {userid}-{username}")
 
 
 def main():
     bot.infinity_polling(timeout=30)
-    # bot.polling(timeout=30)
 
 
 if __name__ == '__main__':
